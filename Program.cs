0f13a11052a9d403ebdee86d46d01233eed499e5
Azeezaarif <azeezaarif@gmail.com>
2024-05-06 15:08:48 +1200

    Final commit

---
+++ Program.cs
@@ -0,0 +1,113 @@
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://example.com",
+                                "http://localhost:4200");
+        });
+});
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseCors();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var todoItems = new List<string>
+            {
+                "Complete assignment",
+                "Buy groceries",
+                "Finish project"
+            };
+
+app.MapGet("/gettasks", () =>
+{
+    return todoItems;
+})
+.WithName("GetTask")
+.WithOpenApi();
+
+app.MapDelete("/removetask", (string taskName) =>
+{
+    todoItems.Remove(taskName);
+    return "ok";
+})
+.WithName("DeleteTask")
+.WithOpenApi();
+
+app.MapPut("/addtask", (string taskName) =>
+{
+    todoItems.Add(taskName);
+    return "ok";
+})
+.WithName("AddTask")
+.WithOpenApi();
+
+app.Run();
+
+
+
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+
+// app.Run();
+
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
+record Person(string taskName);
